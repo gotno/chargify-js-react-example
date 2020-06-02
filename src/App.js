ee85372460159c2cd1c81ebf5b50edf6ac8ec031
Will Floyd <willfloyd@coverhound.com>
2020-06-02 16:24:57 -0700

    Try with all chargify.js setup in the chargify component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ChargifyForm from './components/ChargifyForm';
 
@@ -7,64 +7,12 @@ const App = () => {
   const [extraInput, setExtraInput] = useState('');
   const [token, setToken] = useState('');
 
-  const chargify = new window.Chargify();
-  const chargifyForm = useRef(null);
-
-  useEffect(() => {
-    const baseConfig = {
-      // (i.e. '1a2cdsdn3lkn54lnlkn')
-      publicKey: 'chjs_tnsdj44n87rf7kkvdbm46q9w',
-      // form type (possible values: 'card' or 'bank')
-      type: paymentType || 'card',
-      // points to your Chargify site
-      serverHost: 'https://cyberpolicy-sandbox.chargify.com'
-    }
-
-    if (paymentType === 'bank') {
-      chargify.load({
-        ...baseConfig,
-        // selector where the iframe will be included in the host's HTML (i.e. '#chargify-form')
-        // optional if you have a `selector` on each and every field
-        selector: "#chargify-form",
-      })
-    } else {
-      chargify.load({
-        ...baseConfig,
-        fields: {
-          number: { selector: '#chargify-number' },
-          month: { selector: '#chargify-month' },
-          year: { selector: '#chargify-year' },
-          cvv: { selector: '#chargify-cvv' },
-        },
-      })
-    }
-
-    return () => chargify.unload();
-  }, [paymentType]);
-
-  const handlePaymentTypeChange = (e) => {
-    setPaymentType(e.target.value);
+  const handlePaymentTypeChange = ({ target }) => {
+    setPaymentType(target.value);
   };
 
   const handleInputChange = ({ target }) => {
     setExtraInput(target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    chargify.token(
-      chargifyForm.current,
-
-      (token) => {
-        console.log('{host} token SUCCESS - token: ', token);
-        setToken(token);
-      },
-
-      (error) => {
-        console.log('{host} token ERROR - err: ', error);
-      }
-    );
   };
 
   console.log('APP RENDER');
@@ -80,7 +28,7 @@ const App = () => {
       <label htmlFor="extra">
         extra input: <input name="extra" id="extra" value={extraInput} onChange={handleInputChange}/>
       </label>
-      <ChargifyForm type={paymentType} handleSubmit={handleSubmit} ref={chargifyForm} token={token} />
+      <ChargifyForm type={paymentType} token={token} setToken={setToken} />
     </div>
   );
 }
--- src/components/ChargifyForm.js
+++ src/components/ChargifyForm.js
@@ -1,19 +1,73 @@
-import React, { forwardRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const ChargifyForm = forwardRef(({ handleSubmit, token, type }, ref) => (
-  <form onSubmit={handleSubmit} ref={ref}>
-    <div id="chargify-form"></div>
-    <div id="chargify-number"></div>
-    <div id="chargify-month"></div>
-    <div id="chargify-year"></div>
-    <div id="chargify-cvv"></div>
-    <label>
-      Hidden Token: <input id="host-token" disabled value={token}/>
-    </label>
-    <p>
-      <button type="submit">Submit Host Form</button>
-    </p>
-  </form>
-));
+const ChargifyForm = ({ token, setToken, type }) => {
+  const chargify = new window.Chargify();
+  const chargifyForm = useRef(null);
+
+  useEffect(() => {
+    const baseConfig = {
+      // (i.e. '1a2cdsdn3lkn54lnlkn')
+      publicKey: 'chjs_tnsdj44n87rf7kkvdbm46q9w',
+      // form type (possible values: 'card' or 'bank')
+      type: type || 'card',
+      // points to your Chargify site
+      serverHost: 'https://cyberpolicy-sandbox.chargify.com'
+    }
+
+    if (type === 'bank') {
+      chargify.load({
+        ...baseConfig,
+        // selector where the iframe will be included in the host's HTML (i.e. '#chargify-form')
+        // optional if you have a `selector` on each and every field
+        selector: "#chargify-form",
+      })
+    } else {
+      chargify.load({
+        ...baseConfig,
+        fields: {
+          number: { selector: '#chargify-number' },
+          month: { selector: '#chargify-month' },
+          year: { selector: '#chargify-year' },
+          cvv: { selector: '#chargify-cvv' },
+        },
+      })
+    }
+
+    return () => chargify.unload();
+  }, [type]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    chargify.token(
+      chargifyForm.current,
+
+      (token) => {
+        console.log('{host} token SUCCESS - token: ', token);
+        setToken(token);
+      },
+
+      (error) => {
+        console.log('{host} token ERROR - err: ', error);
+      }
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit} ref={chargifyForm}>
+      <div id="chargify-form"></div>
+      <div id="chargify-number"></div>
+      <div id="chargify-month"></div>
+      <div id="chargify-year"></div>
+      <div id="chargify-cvv"></div>
+      <label>
+        Hidden Token: <input id="host-token" disabled value={token}/>
+      </label>
+      <p>
+        <button type="submit">Submit Host Form</button>
+      </p>
+    </form>
+  );
+};
 
 export default ChargifyForm;
